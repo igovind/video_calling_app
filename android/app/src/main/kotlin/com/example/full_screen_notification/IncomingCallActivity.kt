52076c94149439538bf5cc3e2daf3ea5ec4968ce
Nehal Jaisalmeria <nehal@Nehals-MacBook-Pro.local>
2023-04-30 16:39:19 +0530

    Kotlin Activity

---
+++ android/app/src/main/kotlin/com/example/full_screen_notification/IncomingCallActivity.kt
@@ -0,0 +1,11 @@
+package com.example.full_screen_notification
+
+import android.os.Bundle
+import io.flutter.embedding.android.FlutterActivity
+
+class IncomingCallActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_incoming_call)
+    }
+}
--- android/app/src/main/kotlin/com/example/full_screen_notification/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/full_screen_notification/MainActivity.kt
@@ -1,10 +1,5 @@
 package com.example.full_screen_notification
 
-import androidx.annotation.NonNull
-import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugin.common.MethodChannel
-
 import android.content.Context
 import android.content.ContextWrapper
 import android.content.Intent
@@ -12,22 +7,31 @@ import android.content.IntentFilter
 import android.os.BatteryManager
 import android.os.Build.VERSION
 import android.os.Build.VERSION_CODES
+import androidx.annotation.NonNull
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity() {
-private val CHANNEL = "samples.flutter.dev/battery"
+class MainActivity : FlutterActivity() {
+  private val CHANNEL = "samples.flutter.dev/battery"
 
   override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
     super.configureFlutterEngine(flutterEngine)
     MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
-      call, result ->
+        call,
+        result ->
       if (call.method == "getBatteryLevel") {
-        val batteryLevel = getBatteryLevel()
+        result.success(1)
 
-        if (batteryLevel != -1) {
-          result.success(batteryLevel)
-        } else {
-          result.error("UNAVAILABLE", "Battery level not available.", null)
-        }
+        showIncomingCallScreen(this, "Caller Name")
+
+        // val batteryLevel = getBatteryLevel()
+
+        // if (batteryLevel != -1) {
+        //   result.success(batteryLevel)
+        // } else {
+        //   result.error("UNAVAILABLE", "Battery level not available.", null)
+        // }
       } else {
         result.notImplemented()
       }
@@ -40,10 +44,26 @@ private val CHANNEL = "samples.flutter.dev/battery"
       val batteryManager = getSystemService(Context.BATTERY_SERVICE) as BatteryManager
       batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
     } else {
-      val intent = ContextWrapper(applicationContext).registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
-      batteryLevel = intent!!.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100 / intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
+      val intent =
+          ContextWrapper(applicationContext)
+              .registerReceiver(
+                  null,
+                  IntentFilter(Intent.ACTION_BATTERY_CHANGED),
+              )
+      batteryLevel =
+          intent!!.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100 /
+              intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
     }
 
     return batteryLevel
+  }
+
+  private fun showIncomingCallScreen(context: Context, callerName: String) {
+    val intent =
+        Intent(context, IncomingCallActivity::class.java).apply {
+          putExtra("callerName", callerName)
+          addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        }
+    context.startActivity(intent)
   }
 }
